f2c1538c0b047ce9e09373b8559a19dae07d1e18
Paul Morgen <psmorgen@gmail.com>
2016-05-10 16:27:14 -0700

    Added 9th chords to major and minor

--- MusicTheory3.rb
+++ MusicTheory3.rb
@@ -13,8 +13,8 @@ def initialize
   @lydianArray = [0,2,4,6,7,9,11]
   @mixolydianArray = [0,2,4,5,7,9,10]
   @locrianArray = [0,1,3,5,6,8,10]
-  @majorChord_array = [0,4,7,11]
-  @minorChord_array = [0,3,7,10]
+  @majorChord_array = [0,4,7,11,14]
+  @minorChord_array = [0,3,7,10,14]
   @indexx = @chromatic.find_index(key) #takes instance of the note class converted into string, finds its index in the 'chromatic' array
   @construct = Proc.new do |array|
     structure = []
