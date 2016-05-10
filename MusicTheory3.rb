63485dd3c76a6120fca5fa4acd8326aecdb68c40
Paul Morgen <psmorgen@gmail.com>
2016-05-09 18:16:21 -0700

    Add proc within method as per rubydoc

--- MusicTheory3.rb
+++ MusicTheory3.rb
@@ -10,28 +10,35 @@ def initialize
   @indexx = $chromatic.find_index(key) #takes instance of the note class converted into string, finds its index in the 'chromatic' array
 end
 
-$construct = lambda do |scaleSize,arrayType|
-  @structure = []
-  arrayType.each do |x| #we iterate over the appropriate array's intervals, say first degree of maj scale, 2nd, 3rd...
-    element = (@indexx + x) % scaleSize  #adding each to the index of our starting note
-    component = $chromatic[element]  #associating that index with its place in the chromatic scale
-    structure.push(component) #then add each component of scale/mode/chord by this index into new array
+def construction (scaleSize,arrayType)
+  return Proc.new do
+    structure = []
+    arrayType.each do |x| #we iterate over the appropriate array's intervals, say first degree of maj scale, 2nd, 3rd...
+      element = (@indexx + x) % scaleSize  #adding each to the index of our starting note
+      component = $chromatic[element]  #associating that index with its place in the chromatic scale
+      structure.push(component) #then add each component of scale/mode/chord by this index into new array
+    end
+    puts structure #returns new array containing desired scale/mode/chord
   end
-  return structure #returns new array containing desired scale/mode/chord
 end
 
-def majorScale
-  scaleSize = 12
+majorScale = construction(12, $majorArray)
+majorScale.call
+
+#def majorScale
+  #scaleSize = 12
+  #@arrayType = $majorArray
+  #$construct.call
  # @structure = []
  # $majorArray.each do |x| #we iterate over the appropriate array's intervals, say first degree of maj scale, 2nd, 3rd...
   #  element = (@indexx + x) % scaleSize  #adding each to the index of our starting note
  #   component = $chromatic[element]  #associating that index with its place in the chromatic scale
  #   @structure.push(component) #then add each component of scale/mode/chord by this index into new array
-  end
-  puts @structure #returns new array containing desired scale/mode/chord
-end
+  #end
+  #puts @structure #returns new array containing desired scale/mode/chord
+#end
 
 end
 
 query = Note.new
-query.majorScale
+majorScale
