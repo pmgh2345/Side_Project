9a93eca8da5551935f6fb0905f1fff63a6007c8a
Paul Morgen <psmorgen@gmail.com>
2016-08-05 04:50:39 -0700

    Create hash containing relative values for chord notes to calculate ratios

--- Frequencies.rb
+++ Frequencies.rb
@@ -1,4 +1,25 @@
 require_relative 'MusicTheory3.rb'
 
 chordArray = $output.call.split("--")
-puts chordArray
+#puts chordArray
+
+frequencies = {}
+
+build = Proc.new do
+  firstIndex = $chromatic.find_index(chordArray.first.downcase)
+  chordArray.each do |x|
+    index = $chromatic.find_index(x.downcase)
+    if index < firstIndex
+      index = (12 - (firstIndex + 1)) + index
+    end
+    frequencies[x] = index
+  end
+  frequencies.each do |x,y|
+    difference = frequencies[x] - firstIndex
+    frequencies[x] = difference + 12
+  end
+end
+
+build.call
+
+puts frequencies
